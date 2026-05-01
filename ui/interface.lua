7e4c9db90dae022d886107abd5bca46a113200a2
mushyzkx <mushyzkx@gmail.com>
2026-05-01 01:49:44 -0300

    Update interface.lua

--- ui/interface.lua
+++ ui/interface.lua
@@ -1,12 +1,12 @@
 -- ui/interface.lua
--- Interface Rayfield com Auto Farm All Seas
+-- Interface Rayfield - Auto Farm estilo Redz Hub
 
 return function(Modules)
     local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
     
     local Window = Rayfield:CreateWindow({
-        Name = "Blox Fruits Hub - All Seas",
-        LoadingTitle = "Carregando...",
+        Name = "Blox Fruits Hub - Redz Style",
+        LoadingTitle = "Carregando Hub...",
         LoadingSubtitle = "by mushyzkx",
         ConfigurationSaving = {
             Enabled = true,
@@ -20,15 +20,36 @@ return function(Modules)
     local FarmTab = Window:CreateTab("Auto Farm", 4483345998)
     
     FarmTab:CreateToggle({
-        Name = "Auto Farm (Auto Select)",
+        Name = "Auto Farm Level (Auto Quest)",
         CurrentValue = false,
-        Flag = "AutoFarm",
+        Flag = "AutoFarmLevel",
         Callback = function(Value)
             if Value then
                 Modules.AutoFarm:Start()
             else
                 Modules.AutoFarm:Stop()
             end
+        end
+    })
+    
+    FarmTab:CreateToggle({
+        Name = "Fast Attack / Fast Click",
+        CurrentValue = true,
+        Flag = "FastAttack",
+        Callback = function(Value)
+            Modules.AutoFarm.FastAttack = Value
+        end
+    })
+    
+    FarmTab:CreateSlider({
+        Name = "Fast Click Speed",
+        Range = {0.001, 0.1},
+        Increment = 0.001,
+        Suffix = "s",
+        CurrentValue = 0.01,
+        Flag = "ClickSpeed",
+        Callback = function(Value)
+            Modules.AutoFarm.FastClickSpeed = Value
         end
     })
     
@@ -39,15 +60,6 @@ return function(Modules)
         Flag = "SelectSea",
         Callback = function(Option)
             Modules.AutoFarm.SelectedSea = Option
-        end
-    })
-    
-    FarmTab:CreateToggle({
-        Name = "Fast Attack",
-        CurrentValue = true,
-        Flag = "FastAttack",
-        Callback = function(Value)
-            Modules.AutoFarm.FastAttack = Value
         end
     })
     
@@ -65,6 +77,8 @@ return function(Modules)
         ["Sea 1 - Prison"] = CFrame.new(4854, 5, 734),
         ["Sea 1 - Colosseum"] = CFrame.new(-1428, 7, -3014),
         ["Sea 1 - Magma Village"] = CFrame.new(-5246, 9, 8500),
+        ["Sea 1 - Underwater City"] = CFrame.new(61122, 18, 1567),
+        ["Sea 1 - Fountain City"] = CFrame.new(5129, 59, 4105),
         
         ["Sea 2 - Kingdom of Rose"] = CFrame.new(-394, 120, -3400),
         ["Sea 2 - Green Zone"] = CFrame.new(-2200, 80, -800),
@@ -72,11 +86,17 @@ return function(Modules)
         ["Sea 2 - Snow Mountain"] = CFrame.new(600, 400, -2000),
         ["Sea 2 - Hot and Cold"] = CFrame.new(-5500, 15, -4000),
         ["Sea 2 - Cursed Ship"] = CFrame.new(900, 125, -33000),
+        ["Sea 2 - Ice Castle"] = CFrame.new(2000, 50, -4000),
+        ["Sea 2 - Forgotten Island"] = CFrame.new(-3000, 300, -1000),
         
         ["Sea 3 - Port Town"] = CFrame.new(-300, 80, 6500),
         ["Sea 3 - Hydra Island"] = CFrame.new(5200, 650, 100),
         ["Sea 3 - Great Tree"] = CFrame.new(2300, 50, -6500),
         ["Sea 3 - Floating Turtle"] = CFrame.new(-12000, 350, -8500),
+        ["Sea 3 - Castle on the Sea"] = CFrame.new(-5000, 350, -3000),
+        ["Sea 3 - Haunted Castle"] = CFrame.new(-8500, 50, 6500),
+        ["Sea 3 - Sea of Treats"] = CFrame.new(-1000, 80, -12000),
+        ["Sea 3 - Tiki Outpost"] = CFrame.new(-16000, 10, 500),
     }
     
     for name, cf in pairs(Islands) do
@@ -88,10 +108,10 @@ return function(Modules)
         })
     end
     
-    -- Tab Config
-    local ConfigTab = Window:CreateTab("Config", 4483345998)
+    -- Tab Stats
+    local StatsTab = Window:CreateTab("Stats", 4483345998)
     
-    ConfigTab:CreateSlider({
+    StatsTab:CreateSlider({
         Name = "WalkSpeed",
         Range = {16, 500},
         Increment = 1,
@@ -102,6 +122,21 @@ return function(Modules)
             local Humanoid = LocalPlayer.Character and LocalPlayer.Character:FindFirstChild("Humanoid")
             if Humanoid then
                 Humanoid.WalkSpeed = Value
+            end
+        end
+    })
+    
+    StatsTab:CreateSlider({
+        Name = "JumpPower",
+        Range = {50, 500},
+        Increment = 1,
+        Suffix = "Power",
+        CurrentValue = 50,
+        Flag = "JumpPower",
+        Callback = function(Value)
+            local Humanoid = LocalPlayer.Character and LocalPlayer.Character:FindFirstChild("Humanoid")
+            if Humanoid then
+                Humanoid.JumpPower = Value
             end
         end
     })
